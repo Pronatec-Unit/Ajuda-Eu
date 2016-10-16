b49eae82135c4bea1ce857785fbafbeabf494a53
Wesley <wesley.souza2809@hotmail.com>
2016-10-16 20:57:02 -0300

    APP versão 1.0
    Programado por Victor Barreto e Wesley Andrade Souza

---
+++ apps/AjudaEu/app/src/main/java/themilks/com/ajudaeu/primeiratela.java
@@ -0,0 +1,29 @@
+package themilks.com.ajudaeu;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class primeiratela extends AppCompatActivity {
+    Button botaoA;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.primeira_tela);
+
+        botaoA= (Button) findViewById(R.id.botaoA);
+        botaoA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent tela= new Intent(primeiratela.this, segundatela.class);
+                startActivity(tela);
+                finish();
+            }
+        });
+
+
+    }
+}
---
+++ apps/AjudaEu/app/src/main/java/themilks/com/ajudaeu/segundatela.java
@@ -0,0 +1,13 @@
+package themilks.com.ajudaeu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class segundatela extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.segundatela);
+    }
+}
